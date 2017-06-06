6227a1fbf3530bbf25d0c8ab9813f8ef71d534fc
Ian Wensink <ian@burst-digital.com>
2017-06-07 00:39:08 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = new express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors())
+
+app.get('/locations', (req, res) => {
+  fs.readFile('./example/points.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    let pointsData = {};
+    try {
+      pointsData = JSON.parse(data);
+    } catch(e) {}
+
+    res.json(pointsData);
+  });
+});
+
+app.post('/location', (req, res) => {
+  fs.readFile('./example/points.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    try {
+      const pointsData = JSON.parse(data);
+      const spot = pointsData[req.body.spot];
+      const event = spot.events[req.body.event];
+      const points = event.points;
+      const newPoints = [...points, Object.assign({}, req.body.location, { val: 999999999999 })];
+      event.points = newPoints;
+      fs.writeFile(
+        './example/points.json',
+        JSON.stringify(pointsData),
+        () => {},
+        // () => console.info(`Wrote ${newPoints.length} locations to file`),
+      );
+    } catch(e) {}
+    res.end('OK', 200);
+  });
+});
+
+app.listen(3000);
