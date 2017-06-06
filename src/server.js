d32ff4522588e5c5150c56b063c5043789569797
Ian Wensink <ian@burst-digital.com>
2017-06-07 00:49:44 +0200

    Set up

--- src/server.js
+++ src/server.js
@@ -1,31 +1,34 @@
-const express = require('express');
+const Express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+
 const pathToJson = path.resolve(__dirname, './points.json');
 
-const app = new express();
+const app = new Express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
 app.get('/locations', (req, res) => {
-  fs.readFile(pathToJson, 'utf8', function (err, data) {
-    if (err) throw err;
+  fs.readFile(pathToJson, 'utf8', (err, data) => {
+    if(err) throw err;
     let pointsData = {};
     try {
       pointsData = JSON.parse(data);
-    } catch(e) {}
+    } catch(e) {
+      console.error('Uh oh...');
+    }
 
     res.json(pointsData);
   });
 });
 
 app.post('/location', (req, res) => {
-  fs.readFile(pathToJson, 'utf8', function (err, data) {
-    if (err) throw err;
+  fs.readFile(pathToJson, 'utf8', (err, data) => {
+    if(err) throw err;
     try {
       const pointsData = JSON.parse(data);
       const spot = pointsData[req.body.spot];
@@ -36,9 +39,11 @@ app.post('/location', (req, res) => {
       fs.writeFile(
         pathToJson,
         JSON.stringify(pointsData),
-        () => {},
+        () => console.info('Done'),
       );
-    } catch(e) {}
+    } catch(e) {
+      console.error('Uh oh...');
+    }
     res.end('OK', 200);
   });
 });
